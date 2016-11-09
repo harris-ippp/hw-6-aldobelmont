e3202e098df289c11c4c1fe8905c8237f3e47bf2
aldobelmont <abelmont@uchicago.edu>
2016-11-09 00:13:36 -0600

    Add files via upload

---
+++ e2.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+for line in open("ELECTION_ID"):
+    year = line[:4]
+    code = line.split()[-1]
+
+    data = "http://historical.elections.virginia.gov/elections/download/{}/precincts_include:0/".format(code)
+    resp = requests.get(data)
+    soup = BeautifulSoup(resp.text,"html.parser")
+
+
+    file_name = year+".csv"
+    with open(file_name,"w") as out:
+        out.write(resp.text)
